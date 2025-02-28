fb803b6b30c68d58c87ddb15ade32dc4077ce26d
Kajal <trinity.kajal12@gmail.com>
2025-02-28 18:28:21 +0530

    login design change

--- lib/main.dart
+++ lib/main.dart
@@ -37,10 +37,18 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
-      body: Center(
-        child: CircularProgressIndicator(
-          color: Colors.white,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.blue.shade300, Colors.blue.shade900],
+          ),
+        ),
+        child: Center(
+          child: CircularProgressIndicator(
+            color: Colors.white,
+          ),
         ),
       ),
     );
@@ -51,114 +59,139 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: Text(""),
-      ),
-      body: Column(
-        children: [
-          Container(
-            height: 150,
-            child: Center(
-                child: Image.asset('assets/images/logo.png',
-                  width: 250,
-                )
-            ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.blue.shade300, Colors.blue.shade900],
           ),
-
-          Text("Login To Your Account.",
-            style: TextStyle(
-                fontWeight: FontWeight.w600,
-                fontSize: 18,
-                color: Colors.blue.shade900,
-              ),
-            textAlign: TextAlign.left,
-          ),
-          Container(
-            margin: EdgeInsets.all(20),
-            child: Column(
-              children: [
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: "Enter Username",
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.person_2_outlined),
-                    contentPadding: EdgeInsets.all(5),
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Container(
+              margin: EdgeInsets.all(15),
+              padding: EdgeInsets.all(20), // Add padding inside the container
+              decoration: BoxDecoration(
+                color: Colors.white, // Set container color to white
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 10,
+                    spreadRadius: 2,
                   ),
-                ),
-                SizedBox(height: 30),
-                TextField(
-                  // keyboardType: TextInputType.,
-                  decoration: InputDecoration(
-                    labelText: "Password",
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.lock_outline),
-                    contentPadding: EdgeInsets.all(5),
-                    suffixIcon: IconButton(
-                        onPressed: (){}, 
-                        icon: Icon(Icons.remove_red_eye_outlined),
+                ],
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Container(
+                    height: 150,
+                    child: Center(
+                      child: Image.asset(
+                        'assets/images/logo.png',
+                        width: 250,
+                      ),
                     ),
                   ),
-                  
-                ),
-
-              ],
+                  SizedBox(height: 10),
+                  Text(
+                    "Login To Your Account",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 20,
+                      color: Colors.blue.shade900,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 20),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: "Enter Username",
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.person_2_outlined),
+                      contentPadding: EdgeInsets.all(10),
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  TextField(
+                    obscureText: true, // Hide password
+                    decoration: InputDecoration(
+                      labelText: "Password",
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.lock_outline),
+                      contentPadding: EdgeInsets.all(10),
+                      suffixIcon: IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.remove_red_eye_outlined),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 45,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text("Login"),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue.shade800,
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Text("or sign in with"),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SocialLoginButton(
+                        icon: Icons.facebook_outlined,
+                        color: Colors.blue.shade800,
+                      ),
+                      SizedBox(width: 10),
+                      SocialLoginButton(
+                        icon: Icons.g_mobiledata_outlined,
+                        color: Colors.red.shade800,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
-          SizedBox(height: 20,),
-          SizedBox(
-            width: 320,
-            height: 40,
-            child: ElevatedButton(
-                onPressed: (){},
-                child: Text("login"),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue.shade800,
-                  foregroundColor: Colors.white,
-                ),
-            ),
-          ),
-          SizedBox(height: 40,),
-          Text("or sign in with"),
-          Container(
-            margin: EdgeInsets.all(20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.blue.shade800, width: 1),
-                      borderRadius: BorderRadius.circular(10)
-                  ),
-                  margin: EdgeInsets.symmetric(horizontal: 5),
-                  height: 40,
-                  width: 40,
-                  child: IconButton(
-                      onPressed: (){},
-                      icon: Icon(Icons.facebook_outlined),
-                      color: Colors.blue.shade800,
-                  ),
-                ),
-                Container(
-                  height: 40,
-                  width: 40,
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.red.shade800, width: 1),
-                      borderRadius: BorderRadius.circular(10)
-                  ),
-                  margin: EdgeInsets.symmetric(horizontal: 5),
-                  child: IconButton(
-                      onPressed: (){},
-                      icon: Icon(Icons.g_mobiledata_outlined),
-                      color: Colors.red.shade800,
+        ),
+      ),
+    );
+  }
+}
 
-                  ),
-                ),
+// Social Media Login Button Widget
+class SocialLoginButton extends StatelessWidget {
+  final IconData icon;
+  final Color color;
 
-              ],
-            ),
-          )
-        ],
-      )
+  SocialLoginButton({required this.icon, required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 40,
+      width: 40,
+      decoration: BoxDecoration(
+        border: Border.all(color: color, width: 1),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: IconButton(
+        onPressed: () {},
+        icon: Icon(icon),
+        color: color,
+      ),
     );
   }
 }
